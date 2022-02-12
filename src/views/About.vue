ab2b56db7aa406f53938132899bdb22924041938
cncoksoyler <cncoksoyler@gmail.com>
2022-02-12 10:57:46 +0300

    CountryGuesser component implemented

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,6 +1,16 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
-    
+    <country-guesser />
   </div>
 </template>
+
+<script>
+import CountryGuesser from "../components/CountryGuesser.vue";
+
+export default {
+  components: {
+    CountryGuesser,
+  },
+};
+</script>
